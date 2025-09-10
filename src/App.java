a494ec7915c258fff58e13a7ea7c62ee2b3fcdc0
Avigail <29akravets@gannacademy.org>
2025-09-10 09:15:41 -0400

    I'm done! heyyyyyyyyyy

--- src/App.java
+++ src/App.java
@@ -1,45 +1,69 @@
 import java.util.*;
+
 /**
- *  Basic task (up to B+ level):
- *    - Ask the user for a whole number n.
- *    - Print whether n is EVEN or ODD.
+ * Basic task (up to B+ level):
+ * - Ask the user for a whole number n.
+ * - Print whether n is EVEN or ODD.
  *
- *  Moderate task (up to A level): 
- *    - Ask for a second whole number called max (a positive number).
- *    - If max <= 0, print an error message and skip the percentage.
- *    - If max is greater than 0, compute what percent n is of max as a DECIMAL calculation.
- *      * Example: if n = 37 and max = 90, print: "37 is 41.11111111111111% of 90"
- *    
  *
- *  Challenge task (up to A+ level):
- *    - If max < 0 or max > 100, print "Wrong number!".
- *    - Otherwise:
- *        * Using your decimal percentage from above, decide:
- *          - If n is within ±5% of HALF of max (i.e., |percent - 50| <= 5),
- *            print "Almost halfway!".
- *          - Else if percent < 50, print "Below halfway."
- *          - Else print "Above halfway."
+ * Moderate task (up to A level):
+ * - Ask for a second whole number called max (a positive number).
+ * - If max <= 0, print an error message and skip the percentage.
+ * - If max is greater than 0, compute what percent n is of max as a DECIMAL
+ * calculation.
+ * * Example: if n = 37 and max = 90, print: "37 is 41.11111111111111% of 90"
  * 
- *     Example 1 - below halfway:
- *       Inputs: n = 37, max = 90
- *       Percent = 41.11% (more than 5 below 50)
- *       Output: Below halfway.
+ *
+ * Challenge task (up to A+ level):
+ * - If max < 0 or max > 100, print "Wrong number!". ( STILL MADE IS max <= 0)
+ * - Otherwise:
+ * * Using your decimal percentage from above, decide:
+ * - If n is within ±5% of HALF of max (i.e., |percent - 50| <= 5),
+ * print "Almost halfway!".
+ * - Else if percent < 50, print "Below halfway."
+ * - Else print "Above halfway."
  * 
- *     Example 2 - almost halfway: 
- *       Inputs: n = 48, max = 100
- *       Percent = 48.00% (within 5 of 50)
- *       Output: Almost halfway!
+ * Example 1 - below halfway:
+ * Inputs: n = 37, max = 90
+ * Percent = 41.11% (more than 5 below 50)
+ * Output: Below halfway.
  * 
- *     Example 3: 
- *       Inputs: n = 70, max = 120
- *       Percent = 58.33% (more than 5 above 50)
- *       Output: Above halfway.
+ * Example 2 - almost halfway:
+ * Inputs: n = 48, max = 100
+ * Percent = 48.00% (within 5 of 50)
+ * Output: Almost halfway!
+ * 
+ * Example 3:
+ * Inputs: n = 7, max = 12
+ * Percent = 58.33% (more than 5 above 50)
+ * Output: Above halfway.
  */
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a number:");
+        int n = Integer.valueOf(scanner.nextLine());
+        if (n % 2 == 0) {
+            System.out.println(n + " is even");
+        } else {
+            System.out.println(n + " is odd");
+        }
+        System.out.println("Give another number:");
+        int max = Integer.valueOf(scanner.nextLine());
+        if (max <= 0 || max > 100) {
+            System.out.println("Wrong number!");
+        } else {
+            double answer = (double) n / max * 100;
+            System.out.println(n + " is " + answer + "% of " + max);
+            if (answer - 50 <= 5) {
+                System.out.println("Almost halfway!");
+            } else if (answer < 50) {
+                System.out.println("Below halfway.");
+            } else {
+                System.out.println("Above halfway.");
+            }
 
-
-        
+            //basically two work 
+        }
     }
 }
